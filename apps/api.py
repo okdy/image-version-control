0fed3b58303a6aa8e88bdb0a137f0e261665571b
okdy <roopie123@daum.net>
2019-05-16 21:16:33 +0900

    test view

--- apps/api.py
+++ apps/api.py
@@ -1,7 +1,18 @@
 from flask import Blueprint, render_template
+from database.db import session
+from database.project import Project
+
+from sqlalchemy.orm.exc import NoResultFound
 
 api = Blueprint('api', __name__)
 
-@api.route('/test/<int:data>')
-def test(data):
-	return str(data)
+@api.route('/test/<int:project_id>')
+def test(project_id):
+	
+	try:
+		data = session.query(Project).filter_by(id=project_id).one()
+
+		return data.name
+
+	except NoResultFound:
+		return "no data"
