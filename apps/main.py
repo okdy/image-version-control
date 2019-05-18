9ca6aa530b03b08ffe18ce9f920cd473706cf343
okdy <roopie123@daum.net>
2019-05-18 20:25:56 +0900

    Get projects model

--- apps/main.py
+++ apps/main.py
@@ -1,7 +1,17 @@
 from flask import Blueprint, render_template
+from sqlalchemy import desc
+
+from database.db import session
+from database.project import Project
+from database.draft import Draft
+
+from sqlalchemy.orm.exc import NoResultFound
 
 main = Blueprint('main', __name__)
 
+
 @main.route('/')
 def home():
-	return render_template('index.html')
+	projects = session.query(Project).order_by(desc(Project.id)).all()
+
+	return render_template('index.html', projects=projects)
