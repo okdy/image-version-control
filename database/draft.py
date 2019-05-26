c2572019583f24ee43378573c9f73db2bfe99b63
okdy <roopie123@daum.net>
2019-05-26 22:47:02 +0900

    Add add_draft

--- database/draft.py
+++ database/draft.py
@@ -23,3 +23,15 @@ class Draft(Base):
 
 	def get_all_draft():
 		return session.query(Draft).all()
+
+	#project_id, draft_version headings, description, file
+	def add_draft(project_id, draft_version headings, description, file):
+		newDraft = Draft(
+			project_id=project_id,
+			draft_version=draft_version,
+			headings=headings,
+			description=description,
+			file=file
+		)
+		session.add(newDraft)
+		session.commit()
