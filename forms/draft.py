f4b53f805746dc871c791f4d0a94b5a46685b178
okdy <roopie123@daum.net>
2019-05-26 22:45:39 +0900

    Add draft form

---
+++ forms/draft.py
@@ -0,0 +1,14 @@
+from wtforms import (
+	Form,
+	IntegerField,
+	StringField,
+	FileField,
+	validators as val
+)
+
+
+class DraftForm(Form):
+	ida = IntegerField('id')
+	headings = StringField('headings', [val.Length(min=1)])
+	description = StringField('description', [val.Length(max=140)])
+	file = FileField('file')
