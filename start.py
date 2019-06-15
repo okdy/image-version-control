d60ed6be6c10bf3b98aea3aac66624a6bf4870b7
okdy <roopie123@daum.net>
2019-06-15 21:31:56 +0900

    Add account blueprint

--- start.py
+++ start.py
@@ -2,6 +2,7 @@ from flask import Flask
 
 from apps.main import main
 from apps.api import api
+from apps.auth import auth
 
 app = Flask(__name__)
 
@@ -9,3 +10,4 @@ app.secret_key = 'secret_key_for_debugging_only'
 
 app.register_blueprint(main)
 app.register_blueprint(api)
+app.register_blueprint(auth, url_prefix='/account')
